e897d910ed575601b27fe678a3f4b7ba6b908326
Bruno Pigari Rateiro <105450191+BPRateiro@users.noreply.github.com>
2024-11-22 09:05:48 -0300

    Saving scraped category titles to a JSON

--- gig_finder/gig_finder/spiders/freelancer.py
+++ gig_finder/gig_finder/spiders/freelancer.py
@@ -1,4 +1,6 @@
 import scrapy
+import json
+import re
 
 class FreelancerSpider(scrapy.Spider):
     name = 'freelancer'
@@ -9,12 +11,27 @@ class FreelancerSpider(scrapy.Spider):
         """Extract links associated with the "Websites, IT" section"""
         links = response.xpath('//section[contains(header//h3/text(), "Websites, IT")]/ul/li/a/@href').getall()
 
+        titles = response.xpath('//section[contains(header//h3/text(), "Websites, IT")]/ul/li/a/text()').getall()
+        cleaned_titles = [
+            cleaned_title
+            for title in titles
+            if (cleaned_title := self.clean_title(title))  # Clean once and filter
+        ]
+        with open('categories.json', 'w', encoding='utf-8') as f:
+            json.dump(cleaned_titles, f, ensure_ascii=False, indent=4)
+
         for link in links:
             full_link_start = response.urljoin(link.rstrip('/')) + self.suffix # Starting position
             full_link_finish = response.urljoin(link) + "20" + self.suffix # Skip the 1X pages, because they only redirect to start
 
             yield response.follow(full_link_start, callback=self.parse_job_category)
             yield response.follow(full_link_finish, callback=self.parse_job_category)
+    
+    def clean_title(self, title):
+        """Clean a title by removing extra whitespace and numbers in parentheses."""
+        title = title.strip()  # Remove leading and trailing whitespace
+        title = re.sub(r'\s*\(\d+\)$', '', title)  # Remove numbers in parentheses
+        return title if title else None  # Return None for empty titles
 
     def parse_job_category(self, response):
         """Extract all div elements with a class that contains "JobSearchCard-item"""
