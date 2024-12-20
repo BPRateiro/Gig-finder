58af656ef543620b593e21e9ab406af629d0f713
Bruno Pigari Rateiro <105450191+BPRateiro@users.noreply.github.com>
2024-12-20 11:01:04 -0300

    Added a hyphen in scripts

--- scripts/run_ti_jobs.sh
+++ scripts/run_ti_jobs.sh
@@ -4,5 +4,5 @@
 ./update.sh
 
 # Run the spider for specific categories without historical data
-docker compose -f /home/ec2-user/Gig-finder/docker-compose.yml run gig_finder \
+docker-compose -f /home/ec2-user/Gig-finder/docker-compose.yml run gig_finder \
     scrapy crawl freelancer -a categories='[\"Websites\", \"Data\", \"Artificial\"]' -a historical=False
